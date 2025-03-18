c383ab2c6cdd8c22fee69ace9e39c5bca939b22f
mattyg986 <mattgibbs200@gmail.com>
2025-03-19 05:46:25 +1000

    push progress

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Union
 
 from pydantic import AnyHttpUrl, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     CORS_ORIGINS: List[str] = ["http://localhost:3000", "http://localhost:8000"]
     
     @field_validator("CORS_ORIGINS")
-    def assemble_cors_origins(cls, v: List[str]) -> List[str] | str:
+    def assemble_cors_origins(cls, v: List[str]) -> Union[List[str], str]:
         if isinstance(v, str) and not v.startswith("["):
             return [i.strip() for i in v.split(",")]
         elif isinstance(v, list):
